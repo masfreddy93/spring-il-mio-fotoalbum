3447958a289a0e07c4d56d7e122c3b145f5508cc
Nicola Masiero <103143493+masfreddy93@users.noreply.github.com>
2022-12-21 10:02:57 +0100

    create vue project front-end

---
+++ src/vue-front-end/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <FotoComponent />
+</template>
+
+<script>
+import FotoComponent from './components/FotoComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FotoComponent
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
