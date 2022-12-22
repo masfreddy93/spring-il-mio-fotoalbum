2a1ddf86047595e24317a6843e961ee22b717679
Nicola Masiero <103143493+masfreddy93@users.noreply.github.com>
2022-12-22 16:52:09 +0100

    add a bit of style in front-end

--- src/vue-front-end/src/App.vue
+++ src/vue-front-end/src/App.vue
@@ -18,8 +18,26 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  // text-align: center;
+  // margin-top: 60px;
+  background: linear-gradient(#dfe6e2, #b8c4bd);
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+ul,
+ol,
+menu {
+  list-style-type: none;
+}
+
+a {
+  color: currentColor;
+  text-decoration: none;
 }
 </style>
--- src/vue-front-end/src/components/FotoComponent.vue
+++ src/vue-front-end/src/components/FotoComponent.vue
@@ -1,90 +1,101 @@
 <template>
-  <div class="container">
-    <h2>Fotos</h2>
-    <!-- Input search -->
-    <input 
-      v-model="inputSearch" 
-      type="text" placeholder="Search for title or tag"
-    >
-
-    <!-- All Fotos -->
-    <ul 
-      v-if="fotos.length > 0"
-    >
-      <li 
-        v-for="foto in fotosOnSearch" 
-        :key="foto.id"
+  <main>
+    <div class="container">
+      <h2>Fotos</h2>
+      <!-- Input search -->
+      <p>
+        <input 
+          v-model="inputSearch" 
+          type="text" placeholder="Search for title or tag" class="input-search"
+        >
+      </p>
+  
+      <!-- All Fotos -->
+      <ul 
+        v-if="fotos.length > 0"
       >
-        <!-- Foto details -->
-        <h4>{{ foto.title }}</h4>
-        <img 
-          :src="foto.url" 
-          alt="pic"
+        <li 
+          v-for="foto in fotosOnSearch" 
+          :key="foto.id"
+          class="foto-card"
         >
-        <p>{{ foto.description }}</p>
-        <span>{{ foto.tag }}</span>
-        <br>
-
-        <!-- Comments -->
-        <button 
-          @click="showComments(foto.id)"
-          v-if="show_comments_in_foto_with_id != foto.id"
-        >Show comments</button>
-        <div 
-          v-if="show_comments_in_foto_with_id == foto.id"
-        >
-        <button 
-            @click="show_comments_in_foto_with_id = -1"
-          >Hide comments</button>
-          <ul 
-            v-if="foto.comments && foto.comments.length > 0"
-          >
-            <li
-              v-for="comment in foto.comments"
-              :key="comment.id"
+          <!-- Foto details -->
+          <h4>{{ foto.title }}</h4>
+          <figure>
+            <img 
+              :src="foto.url" 
+              alt="pic"
             >
-              <h4 v-if="comment.author">{{ comment.author }}</h4>
-              <h4 v-else>Anonymous user</h4>
-              <p>{{ comment.text }}</p>
-            </li>
-          </ul>
-          <p 
-            v-else>There are no comments. Write the firtst one!
-          </p>
-          <br>
-
-          <!-- Leave a comment (form)-->
-          <!-- errors -->
-          <p 
-            v-if="errors.length" 
-            style="color: red;"
+          </figure>
+          <p>{{ foto.description }}</p>
+          <span>{{ foto.tag }}</span>
+  
+          <!-- Comments -->
+          <button 
+            @click="showComments(foto.id)"
+            v-if="show_comments_in_foto_with_id != foto.id"
+          >Show comments</button>
+          <div 
+            v-if="show_comments_in_foto_with_id == foto.id"
           >
-            <ul>
-              <li 
-                v-for="error in errors" 
-                :key="error.id"
-              >{{ error }}</li>
-            </ul>
-          </p>
-          <!-- form -->
-          <form 
-            @submit="createComment"
-          >
-            <input v-model="comment_create.author" type="text" name="author" placeholder="Your name">
-            <br>
-            <textarea rows="5" cols="50" v-model="comment_create.text" type="text" name="text" placeholder="Your text"></textarea>
-            <br>
-
-            <input type="submit" value="Comment">
-          </form>
-          <br><br>
-        </div>
-      </li>
-    </ul>
-
-    <!-- If there are no visible fotos in DB -->
-    <p v-else>No fotos in db</p>
-  </div>
+            <button 
+              @click="show_comments_in_foto_with_id = -1"
+            >Hide comments</button>
+            <div class="comments-cont">
+              <ul 
+                v-if="foto.comments && foto.comments.length > 0"
+              >
+                <li
+                  v-for="comment in foto.comments"
+                  :key="comment.id"
+                  class="comment-card"
+                >
+                  <h4 v-if="comment.author">{{ comment.author }}</h4>
+                  <h4 v-else>Anonymous user</h4>
+                  <p>{{ comment.text }}</p>
+                </li>
+              </ul>
+              <p 
+                v-else>There are no comments. Write the firtst one!
+              </p>
+              <br>
+            </div>
+  
+            <!-- Leave a comment (form)-->
+            <!-- errors -->
+            <p 
+              v-if="errors.length" 
+              style="color: red;"
+              class="errors-cont"
+            >
+              <ul>
+                <li 
+                  v-for="error in errors" 
+                  :key="error.id"
+                  class="error-card"
+                >{{ error }}</li>
+              </ul>
+            </p>
+            <!-- form -->
+            <form 
+              @submit="createComment"
+              class="comments-form"
+            >
+              <input v-model="comment_create.author" type="text" name="author" placeholder="Your name">
+              <br>
+              <textarea rows="5" cols="30" v-model="comment_create.text" type="text" name="text" placeholder="Your text"></textarea>
+              <br>
+  
+              <input type="submit" value="Comment">
+            </form>
+          </div>
+        </li>
+      </ul>
+  
+      <!-- If there are no visible fotos in DB -->
+      <p v-else>No fotos in db</p>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -207,37 +218,133 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+
 
 .container{
   max-width: 900px;
   margin: 0 auto;
+
+  h2{
+
+    font-size: 40px;
+    text-transform: uppercase;
+    text-align: center;
+  }
+
+  p{
+    text-align: center;
+
+    .input-search {
+      width: 200px;
+      padding: 5px 10px;
+      margin-bottom: 2rem;
+    }
+  }
+
+  ul{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-basis: 100%;
+    gap: 1rem;
+    .foto-card{
+      border: .5px solid rgb(175, 231, 189);
+      border-radius: 8px;
+      padding: 1rem .5rem;
+      
+      h4{
+        text-align: center;
+        text-transform: uppercase;
+        color: rgb(17, 103, 70);
+        padding: 4px;
+        border-top: .5px solid rgb(100, 167, 138);
+        border-left: .5px solid rgb(202, 250, 214);
+        border-right: .5px solid rgb(202, 250, 214);
+      }
+
+      img {
+        height: 300px;
+        aspect-ratio: 1;
+        object-fit: cover;
+        border-radius: 4px 8px;
+        display: block;
+        margin-bottom: .5rem;
+      }
+
+      p{
+        text-align: justify;
+        font-size: 16px;
+        margin-bottom: .5rem;
+      }
+      
+      span{
+
+        color: rgb(107, 107, 190);
+        font-size: .875rem;
+        font-weight: bold;
+        border-bottom: .5px solid rgb(41, 82, 49);
+        display: block;
+        margin-bottom: .5rem;
+      }
+
+      button{
+        margin-bottom: 1rem;
+      }
+
+      .comments-cont ul{
+        align-items: flex-start;
+        gap: .25rem;
+        .comment-card{
+          padding: .1rem .25rem;
+          border-bottom: .5px solid rgb(138, 131, 131);
+          width: 100%;
+
+          h4{
+            font-size: .80rem;
+            margin-bottom: .1rem;
+            text-align: start;
+            border-top: none;
+          }
+
+          p{
+            font-size: .75rem;
+          }
+        }
+      }
+
+      .errors-cont ul{
+
+        display: block;
+      }
+      .comments-form{
+
+        input, textarea{
+
+          padding: 5px 10px;
+          margin-bottom: .25rem;
+        }
+        
+        input[type="submit"]{
+          cursor: pointer;
+        }
+
+      }
+    }
+  }
+
+  //buttons
+  button{
+
+    width: fit-content;
+    padding: 5px 10px;
+    text-align: justify;
+
+    &:hover{
+
+      cursor: pointer;
+      opacity: .5;
+    }
+  } 
 }
 
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  padding: .5rem;
-  border: .5px solid black;
-  margin-bottom: .5rem;
-}
-
-a {
-  color: #42b983;
-}
-
-img {
-  max-height: 160px;
-}
 </style>
